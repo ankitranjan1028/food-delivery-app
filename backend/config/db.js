615cf44e5b3670718d6077aad22abf8276b18afb
ankitranjan1028 <aankitrn17@gmail.com>
2024-06-22 22:24:19 +0530

    new

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,3 @@ export const  connectDB = async () =>{
     const url = process.env.MONGO_URI
     await mongoose.connect(url).then(()=>console.log("DB Connected"));
 }
-
-
-// add your mongoDB connection string above.
-// Do not use '@' symbol in your databse user's password else it will show an error.
